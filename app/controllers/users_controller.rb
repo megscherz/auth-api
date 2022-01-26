dd00a47b114ae5ac02612d2d2988b7e254a5427c
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-01-26 16:54:25 -0600

    create authentication in the auth-api app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    if current_user
+      user = current_user.user
+      render json: user
+    else
+      render json: [], status: :unauthorized
+    end
+  end
+end
